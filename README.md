ca0ac98948229c7786b7280179b94c5d52b20db0
Alex Makarov <alexpg27@yandex.ru>
2021-06-03 10:18:15 +0300

    Add Allure

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/surh9fw1kqd0vkqk?svg=true)](https://ci.appveyor.com/project/alexpg27/changedate)
--- src/test/java/ru/netology/domain/ChangeDataTest.java
+++ src/test/java/ru/netology/domain/ChangeDataTest.java
@@ -1,6 +1,10 @@
 package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -12,7 +16,15 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ChangeDataTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     public void setUp() {
